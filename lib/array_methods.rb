dab60f7f54e189386cdd3acd4cd2017afa974336
Jamal Farah <moulayjam@gmail.com>
2019-06-18 22:04:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
 def using_include(array, element)
-    array.element.include?()
-end
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    element1 = "wow"
+    element2 = "Tardis"
+    array.include?(element1)
+    array.include?(element2)end
 
 def using_sort(array)
 
